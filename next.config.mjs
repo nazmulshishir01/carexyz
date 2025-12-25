861714fbd3c160ab63e35f0a70472f6340605646
nazmulshishir01 <nazmulshishir01@gmail.com>
2025-12-25 18:50:33 +0600

    docs(env): add .env.local and setup all required connection

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
+
+export default nextConfig;
